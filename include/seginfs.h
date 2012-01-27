221630d7ef4b68ee0a8e5f09f67f7262c44a7732
Kirn Gill <segin2005@gmail.com>
2012-01-27 04:30:44 -0500

    Added pre-existing files to project

---
+++ include/seginfs.h
@@ -0,0 +1,13 @@
+#ifndef SEGINFS_H_INCLUDED
+#define SEGINFS_H_INCLUDED
+
+/* System includes */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+/* Local includes */
+
+#endif // SEGINFS_H_INCLUDED
---
+++ include/structs.h
@@ -0,0 +1,24 @@
+#ifdef  _STRUCTS_H_
+#define _STRUCTS_H_
+
+struct seginfs_sb_fsver {
+    uint16_t v_major;
+    uint16_t v_minor;
+}
+
+struct seginfs_superblock {
+    char            sb_x86_bootjump[4];
+    struct seginfs_sb_fsver
+                    sb_fs_version;
+    uint32_t        sb_blksz;
+    uint64_t        sb_freeblks;  /* free blocks */
+    uint32_t        sb_resblks;   /* superuser reserved blocks */
+    uint64_t        sb_itbl_off;  /* block offset for start of inode table */
+    uint64_t        sb_etbl_off;  /* block offset for start of extents tbl */
+}
+
+struct seginfs_inode {
+
+}
+
+#endif
