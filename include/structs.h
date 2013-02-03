513bb89a47e8622f8338284b90d355444e2936e3
Kirn Gill <segin2005@gmail.com>
2013-02-03 10:40:40 -0500

    Updated main seginfs.h
    Removed the superblock version struct (I won't really need it)
    Added a "magic string" field
    
    Filled in part of the inode format

--- include/structs.h
+++ include/structs.h
@@ -1,16 +1,12 @@
 #ifdef  _STRUCTS_H_
 #define _STRUCTS_H_
 
-struct seginfs_sb_fsver {
-    uint16_t v_major;
-    uint16_t v_minor;
-}
-
 struct seginfs_superblock {
-    char            sb_x86_bootjump[4];
-    struct seginfs_sb_fsver
-                    sb_fs_version;
-    uint32_t        sb_blksz;
+    char            sb_x86_bootjmp[4];  /* Jump to x86 bootstrap */
+	char			sb_magic[8];  /* "seginfs\0" */
+	uint16_t		sb_fs_majver; /* seginfs major version */
+	uint16_t		sb_fs_minver; /* seginfs minor version */
+    uint32_t        sb_blksz;	  /* block size in multiples of 512 bytes */
     uint64_t        sb_freeblks;  /* free blocks */
     uint32_t        sb_resblks;   /* superuser reserved blocks */
     uint64_t        sb_itbl_off;  /* block offset for start of inode table */
@@ -18,7 +14,11 @@ struct seginfs_superblock {
 }
 
 struct seginfs_inode {
-
+	uint64_t		in_ino;
+	uint32_t		in_mode;
+	uint64_t		in_nlink;
+	
+	
 }
 
 #endif
